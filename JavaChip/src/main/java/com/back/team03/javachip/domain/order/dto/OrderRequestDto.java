d809fd4cc6a2b3ee9e9c7b24c7239aca75be4190
gyeongtaggim865-hue <gyeongtaggim865@gmail.com>
2026-03-24 16:35:54 +0900

    이메일 형식 유효성 검사  - Customer class @Email 추가  - OrderController @Valid 추가  - OrderRequestDto email 응답 유효성 추가  - GlobalExceptionHandler 예외처리

--- JavaChip/src/main/java/com/back/team03/javachip/domain/order/dto/OrderRequestDto.java
+++ JavaChip/src/main/java/com/back/team03/javachip/domain/order/dto/OrderRequestDto.java
@@ -1,8 +1,14 @@
 package com.back.team03.javachip.domain.order.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.List;
 
 public record OrderRequestDto(
+
+        @Email(message = "이메일 형식이 올바르지 않습니다.")
+        @NotBlank(message = "이메일은 필수입니다.")
         String email,
         String postalCode,
         String detailAddress,
