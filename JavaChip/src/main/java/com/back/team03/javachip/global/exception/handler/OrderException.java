1e63f160f5be91a39c2784f89c7897dbd67acb51
gyeongtaggim865-hue <gyeongtaggim865@gmail.com>
2026-03-24 14:50:48 +0900

    Exception Class 생성

--- JavaChip/src/main/java/com/back/team03/javachip/global/exception/handler/OrderException.java
+++
@@ -1,7 +0,0 @@
-package com.back.team03.javachip.global.exception.handler;
-
-public class OrderException extends RuntimeException {
-    public OrderException(String message) {
-        super(message);
-    }
-}
--- JavaChip/src/main/java/com/back/team03/javachip/global/exception/handler/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.back.team03.javachip.global.exception.handler;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
